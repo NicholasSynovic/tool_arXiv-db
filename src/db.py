95853211b95ddd5b574594a15d2f59230a33b37c
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-08-31 11:34:10 -0500

    Add code to create the documents table

---
+++ src/db.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+from sqlalchemy import (
+    Column,
+    Engine,
+    MetaData,
+    PrimaryKeyConstraint,
+    String,
+    Table,
+    create_engine,
+)
+
+
+class DB:
+    def __init__(self, path: Path) -> None:
+        self.path: Path = path
+        self.engine: Engine = create_engine(url=f"sqlite:///{path}")
+        self.metadata: MetaData = MetaData()
+
+        self.createTables()
+
+    def createTables(self) -> None:
+        _: Table = Table(
+            "documents",
+            self.metadata,
+            Column("id", String),
+            Column("title", String),
+            Column("comments", String),
+            Column("journal-ref", String),
+            Column("doi", String),
+            Column("report-no", String),
+            Column("license", String),
+            Column("abstract", String),
+            Column("update_date", String),
+            PrimaryKeyConstraint("id"),
+        )
+
+        self.metadata.create_all(checkfirst=True)
