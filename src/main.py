03997340a6943afed60992a422feb47d6187bfd4
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-08-31 18:49:09 -0500

    Add doc strings

--- src/main.py
+++ src/main.py
@@ -10,6 +10,18 @@ from src.db import DB
 
 
 def readJSON(fp: Path, chunksize: int = 10000) -> Iterator[DataFrame]:
+    """
+    Read a JSON file into chunks of DataFrames.
+
+    This function reads a JSON file into chunks of DataFrames, which can be useful for large files or memory-constrained environments.
+
+    :param fp: The path to the JSON file to read.
+    :type fp: Path
+    :param chunksize: The number of rows to include in each chunk. Defaults to 10,000.
+    :type chunksize: int, optional
+    :return: A generator that yields chunks of DataFrames.
+    :rtype: Iterator[DataFrame]
+    """  # noqa: E501
     return pandas.read_json(
         path_or_buf=fp,
         lines=True,
@@ -19,6 +31,16 @@ def readJSON(fp: Path, chunksize: int = 10000) -> Iterator[DataFrame]:
 
 
 def getDocuments(df: DataFrame) -> DataFrame:
+    """
+    Extract a subset of relevant columns from a given DataFrame.
+
+    This function takes a DataFrame as input and returns a new DataFrame containing only the specified columns.
+
+    :param df: The input DataFrame to process.
+    :type df: DataFrame
+    :return: A new DataFrame with the selected columns, including conversion of 'update_date' to datetime format.
+    :rtype: DataFrame
+    """  # noqa: E501
     documentsDF: DataFrame = df[
         [
             "id",
@@ -42,6 +64,20 @@ def getDocuments(df: DataFrame) -> DataFrame:
 
 
 def getAuthors(df: DataFrame, idIncrement: int = 0) -> DataFrame:
+    """
+    Extract author information from a given DataFrame and assign unique IDs.
+
+    This function takes a DataFrame as input, extracts the 'authors_parsed' column,
+    explodes it into separate rows for each author, renames the columns, and assigns
+    a new ID to each row based on the specified increment.
+
+    :param df: The input DataFrame containing author information.
+    :type df: DataFrame
+    :param idIncrement: An integer to increment the IDs by. Defaults to 0.
+    :type idIncrement: int, optional
+    :return: A new DataFrame with the extracted author information and unique IDs.
+    :rtype: DataFrame
+    """  # noqa: E501
     authorsDF: DataFrame = df[["id", "authors_parsed"]]
 
     authorsDF = authorsDF.explode(
@@ -68,6 +104,21 @@ def getAuthors(df: DataFrame, idIncrement: int = 0) -> DataFrame:
 
 
 def getVersions(df: DataFrame, idIncrement: int = 0) -> DataFrame:
+    """
+    Extract version information from a given DataFrame and assign unique IDs.
+
+    This function takes a DataFrame as input, extracts the 'versions' column,
+    explodes it into separate rows for each version, extracts relevant fields ('version', 'created'),
+    converts the 'created' field to datetime format, renames the columns, and assigns
+    a new ID to each row based on the specified increment.
+
+    :param df: The input DataFrame containing version information.
+    :type df: DataFrame
+    :param idIncrement: An integer to increment the IDs by. Defaults to 0.
+    :type idIncrement: int, optional
+    :return: A new DataFrame with the extracted version information and unique IDs.
+    :rtype: DataFrame
+    """  # noqa: E501
     versionsDF: DataFrame = df[["id", "versions"]]
     versionsDF = versionsDF.explode(column="versions", ignore_index=True)
 
@@ -99,6 +150,18 @@ def getVersions(df: DataFrame, idIncrement: int = 0) -> DataFrame:
 
 
 def loadData(dfs: Iterator[DataFrame], db: DB) -> None:
+    """
+    Load data from an iterator of DataFrames into a database.
+
+    This function takes an iterator of DataFrames and a database object as input,
+    processes each DataFrame to extract author, version, and document information,
+    and loads the data into the corresponding tables in the database.
+
+    :param dfs: An iterator yielding DataFrames containing document metadata.
+    :type dfs: Iterator[DataFrame]
+    :param db: A database object with methods for loading data into SQL tables.
+    :type db: DB
+    """  # noqa: E501
     authorIDIncrement: int = 0
     versionsIDIncrement: int = 0
 
