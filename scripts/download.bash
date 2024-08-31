c3085aacfdd662b7ce48ddb2d730229d1c91f625
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-08-31 10:54:47 -0500

    Add script to download data

---
+++ scripts/download.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source optparse.bash
+optparse.define short=p long=path desc="Directory to store dataset" variable=PATH
+source $( optparse.build )
+
+ABS_PATH=$(realpath $PATH)
+
+kaggle datasets download --unzip Cornell-University/arxiv -p $ABS_PATH
