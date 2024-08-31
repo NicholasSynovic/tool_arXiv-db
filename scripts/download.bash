830e7dea8973612ce1dfbe2e27ecdd9104a4b9a3
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-08-31 11:10:00 -0500

    Add json lines tool

--- scripts/download.bash
+++ scripts/download.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source optparse.bash
-optparse.define short=p long=path desc="Directory to store dataset" variable=PATH
+optparse.define short=p long=path desc="Directory to store dataset" variable=PATH default="."
 source $( optparse.build )
 
 ABS_PATH=$(realpath $PATH)
