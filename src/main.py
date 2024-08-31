61c22832aa520378dcbd95024ccebccdc588e170
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-08-31 11:59:29 -0500

    Parse DataFrames for document data

--- src/main.py
+++ src/main.py
@@ -16,11 +16,37 @@ def readJSON(fp: Path, chunksize: int = 10000) -> Iterator[DataFrame]:
     )
 
 
+def getDocuments(df: DataFrame) -> DataFrame:
+    return df[
+        [
+            "id",
+            "title",
+            "comments",
+            "journal-ref",
+            "doi",
+            "report-no",
+            "license",
+            "abstract",
+            "update_date",
+        ]
+    ]
+
+
+def loadData(dfs: Iterator[DataFrame], db: DB) -> None:
+    df: DataFrame
+    for df in dfs:
+        documentsDF: DataFrame = getDocuments(df=df)
+        print(documentsDF.columns)
+        quit()
+
+
 def main() -> None:
     fp: Path = Path("../data/arxiv.jsonlines")
     jr: Iterator[DataFrame] = readJSON(fp=fp)
 
     db: DB = DB(path=Path("test.db"))
+
+    loadData(dfs=jr, db=db)
 
 
 if __name__ == "__main__":
